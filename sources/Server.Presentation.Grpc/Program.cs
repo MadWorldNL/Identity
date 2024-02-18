14b782ab5bf183e915fe83afc34a4f5df1b96d43
Oscar Veldman <oveldman@gmail.com>
2024-02-18 23:00:05 +0100

    Add database of identity

--- sources/Server.Presentation.Grpc/Program.cs
+++ sources/Server.Presentation.Grpc/Program.cs
@@ -1,5 +1,11 @@
+using System.Text;
 using MadWorldNL.Common.AspNetCore;
+using MadWorldNL.Server.Infrastructure.Database;
+using MadWorldNL.Server.Presentation.Grpc;
 using MadWorldNL.Server.Presentation.Grpc.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +13,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 
 builder.AddCommonMadWorldNL();
+
+
+builder.Services.AddDbContext<UserDbContext>(
+    options =>
+        options.UseNpgsql(builder.BuildConnectionString("IdentityConnectionString")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+    };
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -16,5 +41,8 @@ app.MapGrpcService<AuthenticationService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
