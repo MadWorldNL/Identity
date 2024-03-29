6306b5431365da77185a51f11bda7f01776c6390
Oscar Veldman <oveldman@gmail.com>
2024-03-29 20:08:20 +0100

    Finish setup

--- sources/Clients.Admin.Web/Program.cs
+++ sources/Clients.Admin.Web/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Clients.Admin.Web;
+using MadWorldNL.Clients.Admin.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
