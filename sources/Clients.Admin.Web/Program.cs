53a0f22f2e21c4d924b3f0628b56d4cc839c3bfe
Oscar Veldman <oveldman@gmail.com>
2024-04-01 15:31:36 +0200

    Add Radzen

--- sources/Clients.Admin.Web/Program.cs
+++ sources/Clients.Admin.Web/Program.cs
@@ -2,10 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MadWorldNL.Clients.Admin.Web;
 using MadWorldNL.Clients.Identity.Blazor.Shared.Extensions;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddRadzenComponents();
 
 builder.AddIdentity();
 
